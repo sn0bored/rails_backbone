9ad249200600ee1df514704dac5e59bba5d8a412
Daniel Willems <dmkwillems@gmail.com>
2014-03-24 13:30:52 -0400

    create run model, test for require start time

--- db/migrate/20140324171050_create_runs.rb
+++ db/migrate/20140324171050_create_runs.rb
@@ -1,6 +1,8 @@
 class CreateRuns < ActiveRecord::Migration
   def change
     create_table :runs do |t|
+    	t.time :started_at
+    	t.time :stopped_at
 
       t.timestamps
     end
--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe Run do
+  
   it "can creates a new run" do
     run = Run.new
     run.should be_valid
   end
+
+  it "cannot be created without started_at" do
+  	run = Run.create
+  	Run.count.should eq(0)
+	end
 end
